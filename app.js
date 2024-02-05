b3f3ff482e523dda9c72b78404f74551be5d9aa0
sergiocargo <95145986+sergiocargo@users.noreply.github.com>
2024-02-05 18:42:47 -0300

    appp.js

---
+++ app.js
@@ -0,0 +1,26 @@
+//var declaracao de variavel exemplo var anoAtual recebe 2024 
+// Toda vez que for usar variavel sempre escolha let
+//var anoAtual = 2022 <- nao se usar mais assim atualmente
+//console.log(anoAtual)
+//let anoAtual = 2023
+//console.log(anoAtual)
+//anoAtual =(2024)
+//console.log(anoAtual)
+//const anoNascimento = 1965
+//const idade = anoAtual - anoNascimento
+//const novoanoNascimento = anoNascimento
+//const novaIdade = anoAtual - novoanoNascimento
+//console.log(novaIdade)
+//concatenacao isso e quando uma coisa se junta com outra 
+
+const anoAtual = 2024
+const anoNascimento = 1965
+const idade = anoAtual - anoNascimento
+const nome = 'Sergio'
+
+const frase = 'Meu nome '+ nome +' e tenho '+ idade +' anos.'
+
+console.log (frase)
+
+
+
